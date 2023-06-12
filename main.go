fdc9d5c02b14faaefadc2d1baee34d690f6aaecd
Damien CHABRET <damien.chabret@iut-dijon.u-bourgogne.fr>
2023-06-12 20:41:12 +0200

    Changement structure

--- main.go
+++ main.go
@@ -3,41 +3,14 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"text/template"
+	"portfolio/handlers"
 )
-
-func home(w http.ResponseWriter, r *http.Request) {
-	renderTemplates(w, "home")
-}
-
-func about(w http.ResponseWriter, r *http.Request) {
-	renderTemplates(w, "about")
-}
-
-func project(w http.ResponseWriter, r *http.Request) {
-	renderTemplates(w, "project")
-}
-
-func contact(w http.ResponseWriter, r *http.Request) {
-	renderTemplates(w, "contact")
-}
-
-// w =
-// tmpl = template de base
-func renderTemplates(w http.ResponseWriter, tmpl string) {
-	t, err := template.ParseFiles("./templates/" + tmpl + ".page.tmpl")
-
-	if err != nil {
-		fmt.Println("error")
-	}
-	t.Execute(w, nil)
-}
 
 func main() {
 	fmt.Println("Démarage du serveur")
-	http.HandleFunc("/", home)
-	http.HandleFunc("/about", about)
-	http.HandleFunc("/project", project)
-	http.HandleFunc("/contact", contact)
+	http.HandleFunc("/", handlers.Home)
+	http.HandleFunc("/about", handlers.About)
+	http.HandleFunc("/project", handlers.Project)
+	http.HandleFunc("/contact", handlers.Contact)
 	http.ListenAndServe(":5500", nil)
 }

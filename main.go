8d8f7c1c39e55582f8288880a975acaa6138a415
Damien CHABRET <damien.chabret@iut-dijon.u-bourgogne.fr>
2023-06-16 21:55:36 +0200

    Pointage vers dossier CSS

--- main.go
+++ main.go
@@ -8,12 +8,17 @@ import (
 
 func main() {
 	fmt.Println("Démarage du serveur")
+	// Pointe vers les dossiers
 	http.HandleFunc("/", handlers.Home)
 	http.HandleFunc("/me", handlers.Me)
 	http.HandleFunc("/experiences", handlers.Experience)
 	http.HandleFunc("/formation", handlers.Formation)
 	http.HandleFunc("/project", handlers.Project)
 	http.HandleFunc("/contact", handlers.Contact)
+
+	// Pointe vers le dossier css
+	fs := http.FileServer(http.Dir("Assets"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
 
 	http.ListenAndServe(":5500", nil)
 }
